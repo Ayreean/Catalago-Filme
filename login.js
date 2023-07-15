160416c1c7362c32f1600b636763372e797df10b
Carlos Gabriel <carlosgabrielmennabarretocor@gmail.com>
2023-07-14 22:21:21 -0300

    agora foi

--- login.js
+++ login.js
@@ -1,13 +1,20 @@
 // login.js
-function login() {
+function login(event) {
+  event.preventDefault(); // Prevent the default form submission
+
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   // Perform validation here
   if (username === "admin" && password === "admin") {
     // Redirect to main screen
+    console.log("Login successful");
     window.location.href = "main.html";
+  } else if (username === "admin") {
+    alert("Invalid password");
+  } else if (password === "admin") {
+    alert("Invalid username");
   } else {
-    alert("Nome ou senha invalidos");
+    alert("Invalid username and password");
   }
 }
